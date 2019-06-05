995e5bfc2347da6fbfc2bfe90e07eb31d64a0dad
Murilo Campos <murilo.santos@am4.com.br>
2019-06-05 12:27:19 -0300

    Updates

---
+++ precache-manifest.926ea1bb05c012a65b81b92cb2068beb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d18686f30db9fdbdf871d9b442b26c6",
+    "url": "/personagens-starwars/index.html"
+  },
+  {
+    "revision": "fa63549ff3280a0cf86c",
+    "url": "/personagens-starwars/static/css/main.752d6969.chunk.css"
+  },
+  {
+    "revision": "f7c7057c62c452c3d9f6",
+    "url": "/personagens-starwars/static/js/2.c77a4288.chunk.js"
+  },
+  {
+    "revision": "fa63549ff3280a0cf86c",
+    "url": "/personagens-starwars/static/js/main.b3f9feeb.chunk.js"
+  },
+  {
+    "revision": "d558adf9a164039984d1",
+    "url": "/personagens-starwars/static/js/runtime~main.18548005.js"
+  },
+  {
+    "revision": "57a4ed76b98cb5c497523d27b327679d",
+    "url": "/personagens-starwars/static/media/SerifGothicStd-ExtraBold.57a4ed76.woff"
+  },
+  {
+    "revision": "ec3d3e0d3753a1e6adfef3653fdcd74a",
+    "url": "/personagens-starwars/static/media/SerifGothicStd-ExtraBold.ec3d3e0d.woff2"
+  },
+  {
+    "revision": "ddfae4aaac6d90540f99d59e68c757f8",
+    "url": "/personagens-starwars/static/media/loader.ddfae4aa.gif"
+  }
+]);
